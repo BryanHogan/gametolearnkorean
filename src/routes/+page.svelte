4ada9161ca869d4e7052a613164fcea5a29205b5
BryanHogan <bryanhoganme@gmail.com>
2024-11-18 22:00:19 +0100

    testing script

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,3 +6,19 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the  there!!!!</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> This is a test ne.</p>
+
+<p>{numbers.join(' + ')} = {total}</p>
+
+<button onclick={addNumber}>
+	Add a number
+</button>
+
+<script>
+	let numbers = $state([1, 2, 3, 4]);
+	let total = $derived(numbers.reduce((t, n) => t + n, 0));
+
+	function addNumber() {
+		numbers.push(numbers.length + 1);
+		$inspect(numbers);
+	}
+</script>
