a1faca097daec70b7b73374272647bcc853e9a87
BryanHogan <bryanhoganme@gmail.com>
2024-12-01 00:47:42 +0100

    building new page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import words from "$lib/data/words.json";
+
+    let activeWords = $state(1);
+    let pairAmount = $state(5);
+    let selectedPairs = $state([]);
+
+    const shuffledWords = (array) => {
+        return array
+          .sort(() => Math.random() - 0.5);
+    }
+
+    let initializeRound = () => {
+        selectedPairs = shuffledWords(words).slice(0, pairAmount);
+    };
+
+    initializeRound();
+</script>
+
+<div>
+    <h1>Hi there</h1>
+    <ul>
+        {#each selectedPairs as pair}
+            <li>
+                Korean: {pair.korean}, English: {pair.english}
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+</style>
