e4e965ff1a34b6be83f85342924ecf350d8a1b31
BryanHogan <bryanhoganme@gmail.com>
2025-02-04 00:49:28 +0100

    styling and html game start menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -262,33 +262,42 @@
         <h1 class="text-align-center margin-top-m margin-bottom-s">
             Game To Learn Korean
         </h1>
-        <label for={wordPoolLimit}>Select wordpool to use:</label>
-        <select bind:value={wordPoolLimit}>
-            <option value="unlimited">Unlimited</option>
-            <option value="50">50 words</option>
-        </select>
-        <p>Select difficulty levels to include:</p>
-        <label>
-            <input type="checkbox" bind:checked={includeLevel1Cards} />
-            Level 1
-        </label>
-        <label>
-            <input type="checkbox" bind:checked={includeLevel2Cards} />
-            Level 2
-        </label>
-        <label>
-            <input type="checkbox" bind:checked={includeLevel3Cards} />
-            Level 3
-        </label>
+        <div class="options-container">
+            <div class="wordpool-option-container">
+                <label for={wordPoolLimit}>Select wordpool to use:</label>
+                <select bind:value={wordPoolLimit}>
+                    <option value="unlimited">Unlimited</option>
+                    <option value="50">50 words</option>
+                </select>
+            </div>
+            <div class="difficulty-checkbox-container">
+                <p class="margin-bottom-xs">Select difficulty levels to include:</p>
+                <div>
+                    <label>
+                        <input type="checkbox" bind:checked={includeLevel1Cards} />
+                        Level 1
+                    </label>
+                    <label>
+                        <input type="checkbox" bind:checked={includeLevel2Cards} />
+                        Level 2
+                    </label>
+                    <label>
+                        <input type="checkbox" bind:checked={includeLevel3Cards} />
+                        Level 3
+                    </label>
+                </div>
+            </div>
+        </div>
         <div class="margin-inline-auto" style="max-width: 350px; width: 100%">
             <Button type="accent grow" onclick={() => startGame()}
                 >Start Game</Button
             >
         </div>
-
-        <p>Completion rate level 1 cards: {calculateCompletionRate(1)}%</p>
-        <p>Completion rate level 2 cards: {calculateCompletionRate(2)}%</p>
-        <p>Completion rate level 3 cards: {calculateCompletionRate(3)}%</p>
+        <div class="stats-container">
+            <p>Completion rate level 1 cards: {calculateCompletionRate(1)}%</p>
+            <p>Completion rate level 2 cards: {calculateCompletionRate(2)}%</p>
+            <p>Completion rate level 3 cards: {calculateCompletionRate(3)}%</p>
+        </div>
     {/if}
 </div>
 
@@ -321,5 +330,43 @@
         justify-content: center;
         gap: var(--space-s);
         flex: 1;
+    }
+
+    .options-container {
+        max-width: 450px;
+        width: 100%;
+        margin: var(--space-s) auto;
+        &>div {
+            padding: var(--space-m);
+            margin-block: var(--space-m);
+            background-color: var(--color-neutral-800);
+            border-radius: var(--border-radius-m);
+        }
+        & .difficulty-checkbox-container >div {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            gap: var(--space-m);
+            & >* {
+                border: 1px solid var(--color-neutral-700);
+                padding: var(--space-2xs);
+                border-radius: var(--border-radius-s)
+            }
+        }
+    }
+
+    .options-container select,
+    .options-container input[type="checkbox"] {
+        margin-right: var(--space-2xs);
+        background-color: var(--color-neutral-700);
+        border: 1px solid var(--color-accent-800);
+        border-radius: var(--border-radius-s);
+        padding: var(--space-2xs);
+        accent-color: var(--color-accent-500);
+    }
+    .stats-container {
+        margin-inline: auto;
+        margin-block: var(--space-xl);
+        color: var(--color-neutral-300);
     }
 </style>
